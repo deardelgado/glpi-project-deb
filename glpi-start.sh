4e3dff49a78cdaf316acc4a26a08dbeccda3e7a4
Darwin Delgado <104608657+deardelgado@users.noreply.github.com>
2024-03-03 11:36:47 -0500

    Update

--- glpi-start.sh
+++ glpi-start.sh
@@ -58,11 +58,6 @@ TARGET_GLPI_MAJOR_VERSION=$(echo $TARGET_GLPI_VERSION | cut -d. -f1)
 #  echo -e "<VirtualHost *:80>\n\tDocumentRoot /var/www/html/glpi/public\n\n\t<Directory /var/www/html/glpi/public>\n\t\tRequire all granted\n\t\tRewriteEngine On\n\t\tRewriteCond %{REQUEST_FILENAME} !-f\n\t\n\t\tRewriteRule ^(.*)$ index.php [QSA,L]\n\t</Directory>\n\n\tErrorLog /var/log/apache2/error-glpi.log\n\tLogLevel warn\n\tCustomLog /var/log/apache2/access-glpi.log combined\n</VirtualHost>" > /etc/apache2/sites-available/000-default.conf
 #fi
 
-# remove file: install/install.php
-if [ $INSTALL -eq 0 ]; then
-    rm -rf /var/www/html/glpi/install
-fi
-
 #Add scheduled task by cron and enable
 echo "*/2 * * * * www-data /usr/bin/php /var/www/html/glpi/front/cron.php &>/dev/null" > /etc/cron.d/glpi
 #Start cron service
